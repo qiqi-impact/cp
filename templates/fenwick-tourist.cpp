436932552e67bcccd6f536c8ca99267fd70f5105
Michel D'Sa <qiqi.impact.official@gmail.com>
2022-04-22 12:05:47 -0700

    .

---
+++ templates/fenwick-tourist.cpp
@@ -0,0 +1,36 @@
+template <typename T>
+class fenwick {
+ public:
+  vector<T> fenw;
+  int n;
+ 
+  fenwick(int _n) : n(_n) {
+    fenw.resize(n);
+  }
+ 
+  void modify(int x, T v) {
+    while (x < n) {
+      fenw[x] += v;
+      x |= (x + 1);
+    }
+  }
+ 
+  T get(int x) {
+    T v{};
+    while (x >= 0) {
+      v += fenw[x];
+      x = (x & (x + 1)) - 1;
+    }
+    return v;
+  }
+};
+
+const int inf = (int) 1e9;
+ 
+struct node {
+  int a = -inf;
+ 
+  inline void operator += (node &other) {
+    a = max(a, other.a);
+  }
+};
