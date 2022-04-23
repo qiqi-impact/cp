7d7957268cfea98d942771f25ef9f61215cc7200
Michel D'Sa <qiqi.impact.official@gmail.com>
2022-04-23 10:45:12 -0700

    .

--- cf/1659D.cpp
+++ cf/1659D.cpp
@@ -16,25 +16,26 @@ int main() {
         int k;
         cin >> k;
         vi val(k), ret(k, 0), diff(k, 0);
-        int sm = 0, acc = 0;
+        ll sm = 0, acc = 0;
         for (int i = 0;i < k;i++) {
             cin >> val[i];
             sm += val[i];
         }
         for (int i = k-1;i >= 0;i--) {
-            int amt = sm / (i+1);
-            cout << i << " " << amt << " " << (sm % (i+1)) << endl;
+            ll amt = sm / (i+1);
             if (!amt) break;
+            if (i == 0) {
+                ret[i] = 1;
+                break;
+            }
             acc += diff[i];
             val[i] += acc;
             val[i] -= k-i;
             if (i >= amt) diff[i-amt]++;
-            sm -= val[i];
+            sm -= val[i] + amt;
             ret[i] = val[i] ? 1 : 0;
         }
-        for (auto x:ret) {
-            cout << x << " ";
-        }
+        for (auto x:ret) printf("%d ", x);
         cout << endl;
     }
     return 0;
