6877b631ce3a95217d174e22380ba40027bcc9e6
Qiqi Impact <qiqi.impact.official@gmail.com>
2022-06-22 15:26:21 -0700

    .

---
+++ cf/4A.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using vi = vector<int>;
+
+int main() {
+    int n;
+    cin >> n;
+    cout << ((n>=4 && n%2==0) ? "YES" : "NO") << endl;
+    return 0;
+}
