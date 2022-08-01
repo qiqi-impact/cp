fe47021bee0f4eb166953cf51719ecd967ac92e1
Qiqi Impact <qiqi.impact.official@gmail.com>
2022-08-01 15:22:40 -0700

    .

---
+++ leetcode/62.cpp
@@ -0,0 +1,20 @@
+long long nChoosek( long long n, long long k )
+{
+    if (k > n) return 0;
+    if (k * 2 > n) k = n-k;
+    if (k == 0) return 1;
+
+    long long result = n;
+    for( int i = 2; i <= k; ++i ) {
+        result *= (n-i+1);
+        result /= i;
+    }
+    return result;
+}
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        return (int)nChoosek(m+n-2, n-1);
+    }
+};
