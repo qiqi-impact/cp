b027190d25e4f972c945798e1ef72586ae84f327
Qiqi Impact <qiqi.impact.official@gmail.com>
2022-08-02 18:29:26 -0700

    .

--- leetcode/494.py
+++ leetcode/494.py
@@ -1,6 +1,7 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         d = defaultdict(int)
+        #d[k] = number of ways to get a sum of k (so far in the array)
         d[0] = 1
         for n in nums:
             nd = defaultdict(int)
