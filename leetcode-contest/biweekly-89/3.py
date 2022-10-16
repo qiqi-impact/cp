efa836d29aa65612d16775e11b918a8206eb83bb
Qiqi Impact <qiqi.impact.official@gmail.com>
2022-10-15 17:50:33 -0700

    .

--- leetcode-contest/biweekly-89/3.py
+++ leetcode-contest/biweekly-89/3.py
@@ -1,18 +1,7 @@
 class Solution:
     def minimizeArrayValue(self, nums: List[int]) -> int:
-        def can(x):
-            nn = nums[:]
-            for i in range(len(nn)-1, 0, -1):
-                if nn[i] > x:
-                    nn[i-1] += (nn[i] - x)
-            if nn[0] > x:
-                return False
-            return True
-        l, r = 0, 10**9
-        while l < r:
-            mi = (l+r)//2
-            if can(mi):
-                r = mi
-            else:
-                l = mi+1
-        return r
+        mx, cur = 0, 0
+        for i in range(len(nums)):
+            cur += nums[i] 
+            mx = max(mx, ceil(cur / (i+1)))
+        return mx
