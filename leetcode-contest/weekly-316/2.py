4437167aa55352db9f2a0a111eb4713154150b53
Qiqi Impact <qiqi.impact.official@gmail.com>
2022-10-22 21:05:21 -0700

    .

--- leetcode-contest/weekly-316/2.py
+++ leetcode-contest/weekly-316/2.py
@@ -1,9 +1,10 @@
 class Solution:
-    def haveConflict(self, event1: List[str], event2: List[str]) -> bool:
-        def gt(x):
-            return int(x[:2]) * 60 + int(x[3:])
-        a, b = list(map(gt, event1)), list(map(gt, event2))
-        for i in range(24*60):
-            if a[0] <= i <= a[1] and b[0] <= i <= b[1]:
-                return True
-        return False
+    def subarrayGCD(self, nums: List[int], k: int) -> int:
+        ret = 0
+        for i in range(len(nums)):
+            cur = nums[i]
+            for j in range(i, len(nums)):
+                cur = math.gcd(cur, nums[j])
+                if cur == k:
+                    ret += 1
+        return ret
