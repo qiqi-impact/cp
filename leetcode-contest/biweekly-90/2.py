e9090c2736b901c00d0e9e2b4059a25c428919a5
Qiqi Impact <qiqi.impact.official@gmail.com>
2022-10-29 09:14:01 -0700

    .

--- leetcode-contest/biweekly-90/2.py
+++ leetcode-contest/biweekly-90/2.py
@@ -1,17 +1,13 @@
 class Solution:
     def twoEditWords(self, queries: List[str], dictionary: List[str]) -> List[str]:
-        tot = set()
-        for w in dictionary:
-            for i in range(len(w)):
-                for c in string.ascii_lowercase:
-                    t = w[:i] + c + w[i+1:]
-                    tot.add(t)
-        
-        def find(w):
-            for i in range(len(w)):
-                for c in string.ascii_lowercase:
-                    t = w[:i] + c + w[i+1:]
-                    if t in tot:
-                        return True
-        
-        return [q for q in queries if find(q)]
+        ret = []
+        for q in queries:
+            for d in dictionary:
+                dif = 0
+                for i in range(len(q)):
+                    if q[i] != d[i]:
+                        dif += 1
+                if dif <= 2:
+                    ret.append(q)
+                    break
+        return ret
