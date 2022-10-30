fb6d8eb973d2cf308d318c352ea51df5c8c8bc2e
Qiqi Impact <qiqi.impact.official@gmail.com>
2022-10-29 19:08:38 -0700

    .

--- leetcode/487.py
+++ leetcode/487.py
@@ -1,29 +1,14 @@
 class Solution:
     def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
-        l = []
-        lst = None
         ct = 0
-        for n in nums:
-            if n != lst:
-                if lst is not None:
-                    l.append((lst, ct))
-                ct = 1
-                lst = n
-            else:
-                ct += 1
-        l.append((lst, ct))
-        ret = 1
-        for i, (x, y) in enumerate(l):
-            if x == 1:
-                ret = max(ret, y)
-            if x == 0:
-                q = 1
-                if i > 0:
-                    q += l[i-1][1]
-                    ret = max(ret,l[i-1][1]+1)
-                if i < len(l)-1:
-                    q += l[i+1][1]
-                    ret = max(ret,l[i+1][1]+1)
-                if y == 1:
-                    ret = max(ret, q)
+        i, j = 0, -1
+        ret = 0
+        for i in range(len(nums)):
+            while 1:
+                if j+1 == len(nums) or ct + (int)(nums[j+1]==0) >= 2:
+                    break
+                j += 1
+                ct += (int)(nums[j] == 0)
+            ret = max(ret, j - i + 1)
+            ct -= (int)(nums[i] == 0)
         return ret
