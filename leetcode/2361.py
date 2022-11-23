5d9f76aaf275a57d459f2abecd3701bf9b8ffce8
Qiqi Impact <qiqi.impact.official@gmail.com>
2022-11-22 23:28:28 -0800

    .

--- leetcode/2361.py
+++ leetcode/2361.py
@@ -1,28 +1,11 @@
 class Solution:
     def minimumCosts(self, regular: List[int], express: List[int], expressCost: int) -> List[int]:
-        h = [(0, 0, 0)]
-        dist = {(0, 0): 0}
-        
-        ret = [inf] * len(regular)
-        
-        while h:
-            c, a, b = heapq.heappop(h)
-            if dist[a, b] != c:
-                continue
-            if a > 0:
-                ret[a-1] = min(ret[a-1], c)
-            opt = []
-            if b == 1:
-                opt.append([c, a, 1-b])
-            else:
-                opt.append([c+expressCost, a, 1-b])
-            if a != len(regular):
-                if b == 1:
-                    opt.append([c + express[a], a+1, b])
-                else:
-                    opt.append([c + regular[a], a+1, b])
-            for nc, na, nb in opt:
-                if nc < dist.get((na, nb), inf):
-                    dist[na, nb] = nc
-                    heapq.heappush(h, (nc, na, nb))
+        cur = [0, expressCost]
+        ret = []
+        for i in range(len(regular)):
+            cur[0] += regular[i]
+            cur[1] += express[i]
+            cur[0] = min(cur[0], cur[1])
+            cur[1] = min(cur[1], cur[0] + expressCost)
+            ret.append(cur[0])
         return ret
