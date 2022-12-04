49e57ad88d11648dfcf7e52e848d3c57ed15a130
Qiqi Impact <qiqi.impact.official@gmail.com>
2022-12-03 21:43:07 -0800

    .

--- aoc2022/2B.py
+++ aoc2022/2B.py
@@ -3,9 +3,12 @@ with open("in") as f:
     l = {'A': 1, 'B': 2, 'C': 3, 'X': 1, 'Y': 2, 'Z': 3}
     for x in f.read().splitlines():
         a, b = [l[t] for t in x.split(' ')]
-        if b >= 2:
+        if b == 1:
+            ret += (a-2)%3+1
+        if b == 2:
             ret += 3
+            ret += a
         if b == 3:
-            ret += 3
-        ret += (a%3)+1
+            ret += 6
+            ret += (a%3)+1
     print(ret)
