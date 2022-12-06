dbfb6b0e377ac5a22d760619546bea4fcc219b02
Qiqi Impact <qiqi.impact.official@gmail.com>
2022-12-05 21:06:08 -0800

    .

--- aoc2022/5B.py
+++ aoc2022/5B.py
@@ -1,22 +1,17 @@
 with open("in") as f:
     ret = 0
-    
     stacks = [[] for _ in range(10)]
-
     for x in f.read().splitlines():
         for c in range(0, len(x), 4):
             block = x[c:c+4]
             if '[' in block:
                 stacks[c//4].insert(0, (block[1]))
-                # print(stacks)
         xx = x.split(' ')
         if 'move' in xx:
             params = [int(xx[1]), int(xx[3])-1, int(xx[5])-1]
-            # print(params)
-            for x in range(params[0]):
-                a = stacks[params[1]].pop()
-                stacks[params[2]].append(a)
-                # print(stacks)
-    # print(params)
+            a = stacks[params[1]][-params[0]:]
+            stacks[params[2]] += a
+            for i in range(params[0]):
+                stacks[params[1]].pop()
     for i in range(len(stacks)):
         print(stacks[i][-1] if stacks[i] else '', end="")
--- aoc2022/6A.py
+++ aoc2022/6A.py
@@ -4,11 +4,11 @@ with open("in") as f:
     d = defaultdict(int)
     for x in f.read().splitlines():
         for i in range(len(x)):
-            if i >= 14:
-                d[x[i-14]] -= 1
-                if d[x[i-14]] == 0:
-                    del d[x[i-14]]
+            if i >= 4:
+                d[x[i-4]] -= 1
+                if d[x[i-4]] == 0:
+                    del d[x[i-4]]
             d[x[i]] += 1
-            if len(d) == 14:
+            if len(d) == 4:
                 print(i+1)
                 break
