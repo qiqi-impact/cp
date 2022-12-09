bd2861f24769d58d22b9ad4c817fe068eda20216
Qiqi Impact <qiqi.impact.official@gmail.com>
2022-12-08 19:28:43 -0800

    .

--- leetcode/790.py
+++ leetcode/790.py
@@ -1,20 +1,19 @@
+MOD = 10**9+7
+
 class Solution:
     def numTilings(self, n: int) -> int:
-        MOD = 10**9+7
+        
         @cache
-        def dp(a, b):
-            if b < 0 or a < 0:
-                return 0
-            if b == 0:
-                if a == 0:
+        def f(t, b):
+            if t < b:
+                return f(b, t)
+            if t == b:
+                if b <= 1:
                     return 1
-                if a == 1:
+                return (f(t-2, b-2) + f(t-1, b-1) + 2 * f(t-1, b-2)) % MOD
+            else:
+                if b == 0:
                     return 0
-            if a-2 == b:
-                ret = dp(a-2, b)%MOD
-            if a-1 == b:
-                ret = (dp(a-2, b-1) + dp(b, a-2))%MOD
-            if a == b:
-                ret = (dp(a-1, b-1) + dp(a, b-2) + 2 * dp(a-1, b-2))%MOD
-            return ret
-        return dp(n, n)
+                return (f(b, t-2) + f(t-2, b-1)) % MOD
+            
+        return f(n, n)
