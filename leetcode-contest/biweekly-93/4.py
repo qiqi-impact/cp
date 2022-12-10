4845718d711f5aa53064d5223e8c0aee830c3a4a
Qiqi Impact <qiqi.impact.official@gmail.com>
2022-12-10 15:27:31 -0800

    .

--- leetcode-contest/biweekly-93/4.py
+++ leetcode-contest/biweekly-93/4.py
@@ -1,30 +1,24 @@
-# Solution by tonghuikang
-
 class Solution:
     def minimumTotalCost(self, nums1: List[int], nums2: List[int]) -> int:
         n = len(nums1)
-        c = Counter(nums1 + nums2)
-        if max(c.values()) > n:
-            return -1
-
-        sames = []
-        sames.append(nums1[0])
-        sames.append(nums2[0])
-        diffs = []
+        f = defaultdict(int)
+        mx = 0
+        mv = 0
+        sames = 0
+        ret = 0
         
-        res = 0
-        for i,(a,b) in enumerate(zip(nums1[1:], nums2[1:]), start=1):
-            if a == b:
-                res += i
-                sames.append(a)
-                sames.append(a)
-            else:
-                diffs.append(i)
+        for i in range(n):
+            if nums1[i] == nums2[i]:
+                sames += 1
+                f[nums1[i]] += 1
+                if f[nums1[i]] > mx:
+                    mx = f[nums1[i]]
+                    mv = nums1[i]
+                ret += i
                 
-        m = len(sames) // 2
-        c = Counter(sames)
-        maxcnt = max(c.values())
-        if maxcnt > m:
-            res += sum(diffs[:maxcnt-m])
-        
-        return res
+        for i in range(n):
+            if mx * 2 > sames and nums1[i] != nums2[i] and nums1[i] != mv and nums2[i] != mv:
+                ret += i
+                sames += 1
+                
+        return -1 if mx * 2 > sames else ret
