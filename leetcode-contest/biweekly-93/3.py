c4a4cccea51feb6642a23e575a3257f00d2b199e
Qiqi Impact <qiqi.impact.official@gmail.com>
2022-12-10 08:47:35 -0800

    .

--- leetcode-contest/biweekly-93/3.py
+++ leetcode-contest/biweekly-93/3.py
@@ -1,32 +1,9 @@
 class Solution:
     def maxJump(self, stones: List[int]) -> int:
         N = len(stones)
-        def can(x):
-            vis = [0] * N
-            idx = 0
-            while idx != N-1:
-                idx = bisect.bisect(stones, stones[idx] + x) - 1
-                vis[idx] = 1
-            cur = stones[-1]
-            # print(x, vis)
-            for i in range(N-1, -1, -1):
-                if not vis[i]:
-                    diff = cur - stones[i]
-                    if diff > x:
-                        return False
-                    cur = stones[i]
-            return True
-        
-        l = 0
-        for i in range(N-1):
-            l = max(stones[i+1] - stones[i], l)
-        r = stones[-1] - stones[0]
-        while l < r:
-            mi = (l+r)//2
-            if can(mi):
-                r = mi
-            else:
-                l = mi + 1
-        return r
-                
+        ret = stones[1]
+        for i in range(2, N):
+            ret = max(ret, stones[i] - stones[i-2])
+        return ret
+            
             
