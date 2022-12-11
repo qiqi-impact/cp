099ab8c5c1eb26e03242717d33e799a7e72dec03
Qiqi Impact <qiqi.impact.official@gmail.com>
2022-12-10 20:08:04 -0800

    .

--- leetcode-contest/weekly-323/2.py
+++ leetcode-contest/weekly-323/2.py
@@ -1,11 +1,10 @@
 class Solution:
     def longestSquareStreak(self, nums: List[int]) -> int:
-        nums.sort()
         ret = -1
         s = set(nums)
         for n in nums:
             cn = n
-            for i in range(2, len(nums)+10):
+            for i in range(2, 10**5+1):
                 cn *= cn
                 if cn in s:
                     ret = max(ret, i)
