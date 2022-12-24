893781eb0c28826fb0ed2dbaa80fccac809041a9
Qiqi Impact <qiqi.impact.official@gmail.com>
2022-12-23 20:09:25 -0800

    .

--- leetcode/1922.py
+++ leetcode/1922.py
@@ -1,4 +1,15 @@
 class Solution:
     def countGoodNumbers(self, n: int) -> int:
         MOD = 10**9+7
-        return pow(5, (n+1)//2, MOD) * pow(4, n//2, MOD) % MOD
+        pw = [20]
+        for i in range(100):
+            pw.append(pw[-1] * pw[-1] % MOD)
+        
+        k = n//2
+        ret = 1
+        for i in range(100):
+            if k & (1 << i):
+                ret *= pw[i]
+                ret %= MOD
+        
+        return ret * pow(5, n%2) % MOD
