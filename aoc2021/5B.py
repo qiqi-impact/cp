4ccf9cf248d0d557c7e70fb2e45409956e53b784
Qiqi Impact <qiqi.impact.official@gmail.com>
2022-12-26 22:28:58 -0800

    .

---
+++ aoc2021/5B.py
@@ -0,0 +1,39 @@
+from functools import cache
+from collections import defaultdict, deque
+import math
+
+N = 1000
+grid = [[0 for _ in range(N)] for _ in range(N)]
+
+with open('in') as f:
+    for i, l in enumerate(f.read().splitlines()):
+        x, y = l.split(' -> ')
+        a, b = [int(t) for t in x.split(',')]
+        c, d = [int(t) for t in y.split(',')]
+        if a == c:
+            if b > d:
+                b,d = d,b
+            for j in range(b, d+1):
+                grid[a][j] += 1
+        elif b == d:
+            if a > c:
+                a,c = c,a
+            for j in range(a, c+1):
+                grid[j][b] += 1
+        else:
+            tx = c - a
+            ty = d - b
+            steps = abs(tx)
+            x, y = a, b
+            grid[x][y] += 1
+            for i in range(steps):
+                x += 1 if tx > 0 else -1
+                y += 1 if ty > 0 else -1
+                grid[x][y] += 1
+
+ret = 0
+for i in range(N):
+    for j in range(N):
+        if grid[i][j] > 1:
+            ret += 1
+print(ret)
