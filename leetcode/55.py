1722a8be66faa4b2292ae8d62817f554145596f9
Qiqi Impact <qiqi.impact.official@gmail.com>
2022-12-30 20:23:01 -0800

    .

--- leetcode/55.py
+++ leetcode/55.py
@@ -4,6 +4,6 @@ class Solution:
         for i in range(len(nums)):
             if i > mx:
                 return False
-            if i == len(nums)-1:
-                return True
             mx = max(mx, nums[i] + i)
+            if mx >= len(nums)-1:
+                return True
