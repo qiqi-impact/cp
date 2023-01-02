935286e587e92ad49946409cecfd9647b4dd638a
Qiqi Impact <qiqi.impact.official@gmail.com>
2023-01-01 19:46:01 -0800

    .

--- leetcode/376.py
+++ leetcode/376.py
@@ -1,14 +1,9 @@
 class Solution:
     def wiggleMaxLength(self, nums: List[int]) -> int:
-        def wig(pos):
-            cur = nums[0]
-            ln = 1
-            for i in range(1, len(nums)):
-                if nums[i] == cur:
-                    continue
-                if (nums[i] > cur and pos) or (nums[i] < cur and not pos):
-                    ln += 1
-                    pos = not pos
-                cur = nums[i]
-            return ln
-        return max(wig(False), wig(True))
+        neg = pos = 1
+        for i in range(1, len(nums)):
+            if nums[i] > nums[i-1]:
+                neg = pos + 1
+            elif nums[i] < nums[i-1]:
+                pos = neg + 1
+        return max(pos, neg)
