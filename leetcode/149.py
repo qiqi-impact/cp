c448dbd5b9f16fbc40eadba0eb7ec7a350a4cc9f
Qiqi Impact <qiqi.impact.official@gmail.com>
2023-01-07 21:59:29 -0800

    .

--- leetcode/149.py
+++ leetcode/149.py
@@ -1,16 +1,21 @@
 class Solution:
     def maxPoints(self, points: List[List[int]]) -> int:
+        xct = defaultdict(int)
+        for x, _ in points:
+            xct[x] += 1
+        ret = max(xct.values())
+        
         if len(points) <= 2:
             return len(points)
-        ret = 0
+        
         for i in range(len(points)):
             x0, y0 = points[i]
+            slp = defaultdict(int)
             for j in range(i+1, len(points)):
                 x1, y1 = points[j]
-                cur = 2
-                for k in range(j+1, len(points)):
-                    x2, y2 = points[k]
-                    if (x2 - x0) * (y1 - y0) == (x1 - x0) * (y2 - y0):
-                        cur += 1
-                ret = max(ret, cur)
+                if x1 == x0:
+                    continue
+                a, b = x1 - x0, y1 - y0
+                slp[b/a] += 1
+                ret = max(ret, 1+max(slp.values()))
         return ret
