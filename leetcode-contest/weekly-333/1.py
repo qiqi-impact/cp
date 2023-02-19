ec29ee023592b514b4288997167783f58491e26f
Qiqi Impact <qiqi.impact.official@gmail.com>
2023-02-18 20:07:26 -0800

    .

--- leetcode-contest/weekly-333/1.py
+++ leetcode-contest/weekly-333/1.py
@@ -1,9 +1,7 @@
 class Solution:
     def mergeArrays(self, nums1: List[List[int]], nums2: List[List[int]]) -> List[List[int]]:
         d = defaultdict(int)
-        for x, y in nums1:
-            d[x] += y
-        for x, y in nums2:
+        for x, y in nums1 + nums2:
             d[x] += y
         ret = []
         for k in sorted(d.keys()):
