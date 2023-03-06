836cdce84398cca8c495371c4d653037ec1810dd
Qiqi Impact <qiqi.impact.official@gmail.com>
2023-03-05 20:19:54 -0800

    .

--- leetcode/5.py
+++ leetcode/5.py
@@ -1,23 +1,23 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        ret = (0, -1)
-        for i in range(len(s)):
-            for j in range(len(s)):
-                if 0 <= i-j and i+j < len(s):
-                    if s[i-j] != s[i+j]:
-                        break
-                    else:
-                        if ret[1] - ret[0] + 1 < 2*j+1:
-                            ret = (i-j, i+j)
-                else:
-                    break
-            for j in range(len(s)):
-                if 0 <= i-j and i+j+1 < len(s):
-                    if s[i-j] != s[i+j+1]:
-                        break
-                    else:
-                        if ret[1] - ret[0] + 1 < 2*j+2:
-                            ret = (i-j, i+j+1)
-                else:
-                    break
-        return s[ret[0]:ret[1]+1]
+        n = len(s)
+        ret = 0
+        idx = 0
+        for i in range(n):
+            a, b = i, i
+            while a >= 0 and b < n and s[a] == s[b]:
+                l = b - a + 1
+                if l > ret:
+                    ret = l
+                    idx = a
+                a -= 1
+                b += 1
+            a, b = i, i+1
+            while a >= 0 and b < n and s[a] == s[b]:
+                l = b - a + 1
+                if l > ret:
+                    ret = l
+                    idx = a
+                a -= 1
+                b += 1
+        return s[idx:idx+ret]
