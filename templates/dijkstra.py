c170f30dad149614c342fea1fe8f23d5247d4ad1
Qiqi Impact <qiqi.impact.official@gmail.com>
2023-04-22 20:05:00 -0700

    .

---
+++ templates/dijkstra.py
@@ -0,0 +1,15 @@
+START = 0, x
+END = y
+h = [START]
+dist = {START[1]: 0}
+while h:
+    cost, cur = heapq.heappop(h)
+    if dist[cur] != cost:
+        continue
+    if cur == END:
+        return cost
+    for o, w in g[cur]:
+        if dist.get(o, inf) > cost + w:
+            dist[o] = cost + w
+            heapq.heappush((cost + w, o))
+return -1
