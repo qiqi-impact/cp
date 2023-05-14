7943cc92525b46666b4795a09ed4e92b2c534c4d
Qiqi Impact <qiqi.impact.official@gmail.com>
2023-05-13 21:14:13 -0700

    .

--- leetcode-contest/weekly-345/2-greedy.py
+++ leetcode-contest/weekly-345/2-greedy.py
@@ -3,11 +3,4 @@ class Solution:
         ret = [0]
         for i in range(len(derived)-1):
             ret.append(derived[i] ^ ret[-1])
-        if ret[-1] ^ ret[0] == derived[-1]:
-            return True
-        ret = [1]
-        for i in range(len(derived)-1):
-            ret.append(derived[i] ^ ret[-1])
-        if ret[-1] ^ ret[0] == derived[-1]:
-            return True
-        return False
+        return ret[-1] ^ ret[0] == derived[-1]
