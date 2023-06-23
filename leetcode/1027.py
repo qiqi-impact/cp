3747870cf0372fe6ea72daf92e609a417e8c87dd
Qiqi Impact <qiqi.impact.official@gmail.com>
2023-06-22 21:17:02 -0700

    .

---
+++ leetcode/1027.py
@@ -0,0 +1,10 @@
+class Solution:
+    def longestArithSeqLength(self, nums: List[int]) -> int:
+        d = {None: 1}
+        seen = set()
+        for i, x in enumerate(nums):
+            for y in seen:
+                df = x - y
+                d[x, df] = max(d.get((x, df), 0), d.get((y, df), 1) + 1)
+            seen.add(x)
+        return max(d.values())
