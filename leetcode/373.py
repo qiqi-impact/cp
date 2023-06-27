1b1a27977334e516f4595532826415d29232ba39
Qiqi Impact <qiqi.impact.official@gmail.com>
2023-06-26 22:23:15 -0700

    .

--- leetcode/373.py
+++ leetcode/373.py
@@ -1,12 +1,13 @@
 class Solution:
     def kSmallestPairs(self, nums1: List[int], nums2: List[int], k: int) -> List[List[int]]:
-        h = []
-        for i, x in enumerate(nums1):
-            h.append((x+nums2[0], i, 0))
         ret = []
-        while len(ret) < k and h:
-            x, y, z = heapq.heappop(h)
-            ret.append((nums1[y], nums2[z]))
-            if z != len(nums2)-1:
-                heapq.heappush(h, (nums1[y]+nums2[z+1], y, z+1))
+        h = [(nums1[0]+nums2[0], 0, 0)]
+        for _ in range(k):
+            if not h: break
+            _, i, j = heapq.heappop(h)
+            ret.append((nums1[i], nums2[j]))
+            if j == 0 and i != len(nums1)-1:
+                heapq.heappush(h, (nums1[i+1]+nums2[j], i+1, j))
+            if j != len(nums2)-1:
+                heapq.heappush(h, (nums1[i]+nums2[j+1], i, j+1))
         return ret
