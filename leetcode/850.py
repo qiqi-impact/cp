76efc3a4aed0dbb00630218252c7e4418f1d0174
Qiqi Impact <qiqi.impact.official@gmail.com>
2023-07-05 00:44:08 -0700

    .

--- leetcode/850.py
+++ leetcode/850.py
@@ -1,44 +1,30 @@
 class Solution:
     def rectangleArea(self, rectangles: List[List[int]]) -> int:
-        ev = {}
-        for i, (a, b, c, d) in enumerate(rectangles):
-            if a not in ev: ev[a] = {}
-            if c not in ev: ev[c] = {}
-            if b not in ev[a]: ev[a][b] = 0
-            if d not in ev[a]: ev[a][d] = 0
-            if b not in ev[c]: ev[c][b] = 0
-            if d not in ev[c]: ev[c][d] = 0
-            
-            ev[a][b] += 1
-            ev[a][d] -= 1
-            ev[c][b] -= 1
-            ev[c][d] += 1
-            
-        # print(ev)
-        
-        cev = {}
-        lx = None
+        ev = defaultdict(list)
+        for a, b, c, d in rectangles:
+            ev[a].append((b, 1))
+            ev[a].append((d, -1))
+            ev[c].append((b, -1))
+            ev[c].append((d, 1))
+        seg = {}
+        prv = 0
+        width = 0
         ret = 0
-        for x in sorted(ev.keys()):
-            if lx is not None:
-                ret += (x - lx) * ln
-            for y in ev[x]:
-                if y not in cev: cev[y] = 0
-                cev[y] += ev[x][y]
-                
+        for k in sorted(ev.keys()):
+            ret += width * (k - prv)
+            for x, d in ev[k]:
+                seg[x] = seg.get(x, 0) + d
+                if seg[x] == 0:
+                    del seg[x]
+            width = 0
             st = 0
-            op = None
-            ln = 0
-            for y in sorted(cev.keys()):
-                st += cev[y]
-                if op is None and st > 0:
-                    op = y
-                elif op is not None and st == 0:
-                    ln += y - op
-                    op = None
-                    
-            lx = x
-            
+            left = None
+            for x in sorted(seg.keys()):
+                st += seg[x]
+                if st > 0 and left is None:
+                    left = x
+                if st == 0 and left is not None:
+                    width += x - left
+                    left = None
+            prv = k
         return ret % (10**9+7)
-            
-            
