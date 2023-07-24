5af8ff9fd135ed0d68cdd29309b1422468a2e583
Qiqi Impact <qiqi.impact.official@gmail.com>
2023-07-23 17:52:22 -0700

    .

--- other/foo.py
+++ other/foo.py
@@ -1,20 +1,12 @@
-from functools import cache
+def closest_left_larger(arr):
+    ret = [None] * len(arr)
 
-class Solution:
-    def minimizeConcatenatedLength(self, words):
-        @cache
-        def dp(idx, left, right):
-            if idx == len(words):
-                return 0
-            w = words[idx]
-            l, r = words[idx][0], words[idx][-1]
-            # print(idx, left, right, min(len(w)-1 + int(r != left) + dp(idx+1, l, right), len(w)-1 + int(l != right) + dp(idx+1, left, r)))
-            return min(len(w)-1 + int(r != left) + dp(idx+1, l, right), len(w)-1 + int(l != right) + dp(idx+1, left, r))
-        return len(words[0]) + dp(1, words[0][0], words[0][-1])
+    st = []
+    for i in range(len(arr)-1, -1, -1):
+        while st and arr[i] > arr[st[-1]]:
+            cur = st.pop()
+            ret[cur] = i
+        st.append(i)
+    return ret
 
-for tc in [
-    ["aa","ab","bc"],
-    ["ab","b"],
-    ["aaa","c","aba"],
-]:
-    print(Solution().minimizeConcatenatedLength(tc))
+print(closest_left_larger([4,3,1,2,4,8,6,7]))
