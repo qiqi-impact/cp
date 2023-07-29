936a9c935d1e472d1f8d986c58adcd207804e576
Qiqi Impact <qiqi.impact.official@gmail.com>
2023-07-29 16:46:51 -0700

    .

---
+++ cf/1847D.cpp
@@ -0,0 +1,339 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+
+namespace output {
+	void pr(int x) { cout << x; }
+	void pr(long x) { cout << x; }
+	void pr(ll x) { cout << x; }
+	void pr(unsigned x) { cout << x; }
+	void pr(unsigned long x) { cout << x; }
+	void pr(unsigned long long x) { cout << x; }
+	void pr(float x) { cout << x; }
+	void pr(double x) { cout << x; }
+	void pr(ld x) { cout << x; }
+	void pr(char x) { cout << x; }
+	void pr(const char* x) { cout << x; }
+	void pr(const string& x) { cout << x; }
+	void pr(bool x) { pr(x ? "true" : "false"); }
+	template<class T> void pr(const complex<T>& x) { cout << x; }
+	
+	template<class T1, class T2> void pr(const pair<T1,T2>& x);
+	template<class T> void pr(const T& x);
+	
+	template<class T, class... Ts> void pr(const T& t, const Ts&... ts) { 
+		pr(t); pr(ts...); 
+	}
+	template<class T1, class T2> void pr(const pair<T1,T2>& x) { 
+		pr("{",x.f,", ",x.s,"}"); 
+	}
+	template<class T> void pr(const T& x) { 
+		pr("{"); // const iterator needed for vector<bool>
+		bool fst = 1; for (const auto& a: x) pr(!fst?", ":"",a), fst = 0; 
+		pr("}");
+	}
+	
+	void ps() { pr("\n"); } // print w/ spaces
+	template<class T, class... Ts> void ps(const T& t, const Ts&... ts) { 
+		pr(t); if (sizeof...(ts)) pr(" "); ps(ts...); 
+	}
+	
+	void pc() { pr("]\n"); } // debug w/ commas
+	template<class T, class... Ts> void pc(const T& t, const Ts&... ts) { 
+		pr(t); if (sizeof...(ts)) pr(", "); pc(ts...); 
+	}
+	#define dbg(x...) pr("[",#x,"] = ["), pc(x);
+}
+
+using namespace output;
+
+class segtree {
+ public:
+  struct node {
+    // don't forget to set default value (used for leaves)
+    // not necessarily neutral element!
+    long long add = 0;
+    long long sum = 0;
+ 
+    void apply(int l, int r, long long v) {
+    	add += v;
+		sum += v * (r - l + 1);
+		// dbg(l, r, v, add);
+    }
+  };
+ 
+  node unite(const node &a, const node &b) const {
+    node res;
+    res.sum = a.sum + b.sum;
+    return res;
+  }
+ 
+  inline void push(int x, int l, int r) {
+    int y = (l + r) >> 1;
+    int z = x + ((y - l + 1) << 1);
+    // push from x into (x + 1) and z
+    if (tree[x].add != 0) {
+      tree[x + 1].apply(l, y, tree[x].add);
+      tree[z].apply(y + 1, r, tree[x].add);
+      tree[x].add = 0;
+    }
+  }
+ 
+  inline void pull(int x, int z) {
+    tree[x] = unite(tree[x + 1], tree[z]);
+  }
+ 
+  int n;
+  vector<node> tree;
+ 
+  void build(int x, int l, int r) {
+    if (l == r) {
+      return;
+    }
+    int y = (l + r) >> 1;
+    int z = x + ((y - l + 1) << 1);
+    build(x + 1, l, y);
+    build(z, y + 1, r);
+    pull(x, z);
+  }
+ 
+  template <typename M>
+  void build(int x, int l, int r, const vector<M> &v) {
+    if (l == r) {
+      tree[x].apply(l, r, v[l]);
+      return;
+    }
+    int y = (l + r) >> 1;
+    int z = x + ((y - l + 1) << 1);
+    build(x + 1, l, y, v);
+    build(z, y + 1, r, v);
+    pull(x, z);
+  }
+ 
+  node get(int x, int l, int r, int ll, int rr) {
+    if (ll <= l && r <= rr) {
+      return tree[x];
+    }
+    int y = (l + r) >> 1;
+    int z = x + ((y - l + 1) << 1);
+    push(x, l, r);
+    node res{};
+    if (rr <= y) {
+      res = get(x + 1, l, y, ll, rr);
+    } else {
+      if (ll > y) {
+        res = get(z, y + 1, r, ll, rr);
+      } else {
+        res = unite(get(x + 1, l, y, ll, rr), get(z, y + 1, r, ll, rr));
+      }
+    }
+    pull(x, z);
+    return res;
+  }
+ 
+  template <typename... M>
+  void modify(int x, int l, int r, int ll, int rr, const M&... v) {
+    if (ll <= l && r <= rr) {
+      tree[x].apply(l, r, v...);
+      return;
+    }
+    int y = (l + r) >> 1;
+    int z = x + ((y - l + 1) << 1);
+    push(x, l, r);
+    if (ll <= y) {
+      modify(x + 1, l, y, ll, rr, v...);
+    }
+    if (rr > y) {
+      modify(z, y + 1, r, ll, rr, v...);
+    }
+    pull(x, z);
+  }
+ 
+  int find_first_knowingly(int x, int l, int r, const function<bool(const node&)> &f) {
+    if (l == r) {
+      return l;
+    }
+    push(x, l, r);
+    int y = (l + r) >> 1;
+    int z = x + ((y - l + 1) << 1);
+    int res;
+    if (f(tree[x + 1])) {
+      res = find_first_knowingly(x + 1, l, y, f);
+    } else {
+      res = find_first_knowingly(z, y + 1, r, f);
+    }
+    pull(x, z);
+    return res;
+  }
+ 
+  int find_first(int x, int l, int r, int ll, int rr, const function<bool(const node&)> &f) {
+    if (ll <= l && r <= rr) {
+      if (!f(tree[x])) {
+        return -1;
+      }
+      return find_first_knowingly(x, l, r, f);
+    }
+    push(x, l, r);
+    int y = (l + r) >> 1;
+    int z = x + ((y - l + 1) << 1);
+    int res = -1;
+    if (ll <= y) {
+      res = find_first(x + 1, l, y, ll, rr, f);
+    }
+    if (rr > y && res == -1) {
+      res = find_first(z, y + 1, r, ll, rr, f);
+    }
+    pull(x, z);
+    return res;
+  }
+ 
+  int find_last_knowingly(int x, int l, int r, const function<bool(const node&)> &f) {
+    if (l == r) {
+      return l;
+    }
+    push(x, l, r);
+    int y = (l + r) >> 1;
+    int z = x + ((y - l + 1) << 1);
+    int res;
+    if (f(tree[z])) {
+      res = find_last_knowingly(z, y + 1, r, f);
+    } else {
+      res = find_last_knowingly(x + 1, l, y, f);
+    }
+    pull(x, z);
+    return res;
+  }
+ 
+  int find_last(int x, int l, int r, int ll, int rr, const function<bool(const node&)> &f) {
+    if (ll <= l && r <= rr) {
+      if (!f(tree[x])) {
+        return -1;
+      }
+      return find_last_knowingly(x, l, r, f);
+    }
+    push(x, l, r);
+    int y = (l + r) >> 1;
+    int z = x + ((y - l + 1) << 1);
+    int res = -1;
+    if (rr > y) {
+      res = find_last(z, y + 1, r, ll, rr, f);
+    }
+    if (ll <= y && res == -1) {
+      res = find_last(x + 1, l, y, ll, rr, f);
+    }
+    pull(x, z);
+    return res;
+  }
+ 
+  segtree(int _n) : n(_n) {
+    assert(n > 0);
+    tree.resize(2 * n - 1);
+    build(0, 0, n - 1);
+  }
+ 
+  template <typename M>
+  segtree(const vector<M> &v) {
+    n = v.size();
+    assert(n > 0);
+    tree.resize(2 * n - 1);
+    build(0, 0, n - 1, v);
+  }
+ 
+  node get(int ll, int rr) {
+    assert(0 <= ll && ll <= rr && rr <= n - 1);
+    return get(0, 0, n - 1, ll, rr);
+  }
+ 
+  node get(int p) {
+    assert(0 <= p && p <= n - 1);
+    return get(0, 0, n - 1, p, p);
+  }
+ 
+  template <typename... M>
+  void modify(int ll, int rr, const M&... v) {
+    assert(0 <= ll && ll <= rr && rr <= n - 1);
+    modify(0, 0, n - 1, ll, rr, v...);
+  }
+ 
+  // find_first and find_last call all FALSE elements
+  // to the left (right) of the sought position exactly once
+ 
+  int find_first(int ll, int rr, const function<bool(const node&)> &f) {
+    assert(0 <= ll && ll <= rr && rr <= n - 1);
+    return find_first(0, 0, n - 1, ll, rr, f);
+  }
+ 
+  int find_last(int ll, int rr, const function<bool(const node&)> &f) {
+    assert(0 <= ll && ll <= rr && rr <= n - 1);
+    return find_last(0, 0, n - 1, ll, rr, f);
+  }
+};
+
+void solve() {
+    int n, m, q;
+	cin >> n >> m >> q;
+	vi a(n), prio(n), ord(n);
+	
+	vector<set<int>> st(n), ed(n);
+	string s;
+	cin >> s;
+	int ct = 0, valid = 0;
+	for (int i = 0;i < n;i++) {
+		a[i] = s[i] - '0';
+		ct += a[i];
+	}
+	for (int i = 0;i < m;i++) {
+		int x, y;
+		cin >> x >> y;
+		st[x-1].insert(i);
+		ed[y-1].insert(i);
+	}
+	set<int> cur;
+	cur.insert(INT_MAX);
+	for (int i = 0;i < n;i++) {
+		cur.insert(st[i].begin(), st[i].end());
+		prio[i] = *(cur.begin());
+		if (prio[i] != INT_MAX) valid++;
+		for (auto x : ed[i]) cur.erase(x);
+	}
+	for (int i = 0;i < n;i++) ord[i] = i;
+	sort(ord.begin(), ord.end(), [&](int x, int y) {
+		if (prio[x] < prio[y]) return true;
+		if (prio[x] > prio[y]) return false;
+		return x < y;
+	});
+	vi inv(n);
+	for (int i = 0;i < n;i++) inv[ord[i]] = i;
+	vi b(n);
+	for (int i = 0;i < n;i++) b[inv[i]] = a[i];
+
+	segtree seg(b);
+
+	for (int i = 0;i < q;i++) {
+		int p;
+		cin >> p;
+		p--;
+		// dbg(p);
+		b[inv[p]] = 1-b[inv[p]];
+		ct += b[inv[p]] ? 1 : -1;
+		seg.modify(inv[p], inv[p], b[inv[p]] ? 1 : -1);
+		if (!ct) {
+			cout << 0 << endl;
+			continue;
+		}
+		int h = seg.get(0, min(valid, ct)-1).sum;
+		cout << min(valid, ct) - h << endl;
+	}
+
+	// dbg(b, seg.get(0, 4).sum);
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    solve();
+    return 0;
+}
