f6a6eca25d779cf534e66068d4c597e6102b1406
Qiqi Impact <qiqi.impact.official@gmail.com>
2023-08-05 21:11:47 -0700

    .

--- leetcode-contest/weekly-357/2.py
+++ leetcode-contest/weekly-357/2.py
@@ -1,20 +1,7 @@
 class Solution:
     def canSplitArray(self, nums: List[int], m: int) -> bool:
-        pf = [0]
-        for x in nums:
-            pf.append(pf[-1] + x)
-        
-        @cache
-        def valid(l, r):
-            return l == r or pf[r+1] - pf[l] >= m
-        
-        @cache
-        def can(l, r):
-            if l == r:
+        if len(nums) <= 2: return True
+        for x, y in pairwise(nums):
+            if x+y >= m:
                 return True
-            for i in range(l, r):
-                if valid(l, i) and valid(i+1, r) and can(l, i) and can(i+1, r):
-                    return True
-            return False
-        
-        return can(0, len(nums)-1)
+        return False
