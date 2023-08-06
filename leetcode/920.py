c95aca2a2d66170381a799fb4ef19fdba20cf02e
Qiqi Impact <qiqi.impact.official@gmail.com>
2023-08-05 22:21:20 -0700

    .

--- leetcode/920.py
+++ leetcode/920.py
@@ -5,12 +5,13 @@ class Solution:
         @cache
         def dp(a, b):
             if a == goal:
-                return b == n
-            ret = 0
-            if b > k:
-                ret += (b-k) * dp(a+1, b)
-            if n > b:
-                ret += (n-b) * dp(a+1, b+1)
+                ret = int(b == n)
+            else:
+                ret = 0
+                if b > k:
+                    ret += (b-k) * dp(a+1, b)
+                if n > b:
+                    ret += (n-b) * dp(a+1, b+1)
             return ret%MOD
 
         return dp(0, 0)
