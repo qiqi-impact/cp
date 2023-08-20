b01895e226344de75d2f362d4c03bb37dfff5925
Qiqi Impact <qiqi.impact.official@gmail.com>
2023-08-19 21:14:30 -0700

    .

--- leetcode-contest/weekly-359/3.py
+++ leetcode-contest/weekly-359/3.py
@@ -5,7 +5,8 @@ class Solution:
         op = 0
         dp = [0] * mx
         for t in range(mx):
-            dp[t] = dp[t-1]
+            if t != 0:
+                dp[t] = dp[t-1]
             while op < len(offers) and offers[op][1] == t:
                 a, b, c = offers[op]
                 dp[t] = max((0 if a == 0 else dp[a-1]) + c, dp[t])
