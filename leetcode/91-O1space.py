9aadbe73df2e6782ac20d354867eafef27acef5c
Qiqi Impact <qiqi.impact.official@gmail.com>
2023-08-21 16:46:45 -0700

    .

--- leetcode/91-O1space.py
+++ leetcode/91-O1space.py
@@ -1,13 +1,14 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        dp = [0] * (len(s)+1)
-        dp[0] = 1
+        a, b = 0, 1
         for i in range(len(s)):
             x = int(s[i])
+            c = 0
             if 1 <= x <= 26:
-                dp[i+1] += dp[i]
+                c += b
             if i > 0 and s[i-1] != '0':
                 x += int(s[i-1]) * 10
                 if 1 <= x <= 26:
-                    dp[i+1] += dp[i-1]
-        return dp[-1]
+                    c += a
+            a, b = b, c
+        return b
