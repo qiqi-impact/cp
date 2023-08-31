957051f0e55e98733b9c38ec884bdcfbf4a694b3
Qiqi Impact <qiqi.impact.official@gmail.com>
2023-08-31 02:14:47 -0700

    .

---
+++ cf/1851G.cpp
@@ -0,0 +1,194 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using pii = pair<int, int>;
+
+namespace output {
+	void pr(int x) { cout << x; }
+	void pr(long x) { cout << x; }
+	void pr(ll x) { cout << x; }
+	void pr(unsigned x) { cout << x; }
+	void pr(unsigned long x) { cout << x; }
+	void pr(unsigned long long x) { cout << x; }
+	void pr(float x) { cout << x; }
+	void pr(double x) { cout << x; }
+	void pr(ld x) { cout << x; }
+	void pr(char x) { cout << x; }
+	void pr(const char* x) { cout << x; }
+	void pr(const string& x) { cout << x; }
+	void pr(bool x) { pr(x ? "true" : "false"); }
+	template<class T> void pr(const complex<T>& x) { cout << x; }
+	
+	template<class T1, class T2> void pr(const pair<T1,T2>& x);
+	template<class T> void pr(const T& x);
+	
+	template<class T, class... Ts> void pr(const T& t, const Ts&... ts) { 
+		pr(t); pr(ts...); 
+	}
+	template<class T1, class T2> void pr(const pair<T1,T2>& x) { 
+		pr("{",x.first,", ",x.second,"}"); 
+	}
+	template<class T> void pr(const T& x) { 
+		pr("{"); // const iterator needed for vector<bool>
+		bool fst = 1; for (const auto& a: x) pr(!fst?", ":"",a), fst = 0; 
+		pr("}");
+	}
+	
+	void ps() { pr("\n"); } // print w/ spaces
+	template<class T, class... Ts> void ps(const T& t, const Ts&... ts) { 
+		pr(t); if (sizeof...(ts)) pr(" "); ps(ts...); 
+	}
+	
+	void pc() { pr("]\n"); } // debug w/ commas
+	template<class T, class... Ts> void pc(const T& t, const Ts&... ts) { 
+		pr(t); if (sizeof...(ts)) pr(", "); pc(ts...); 
+	}
+	#define dbg(x...) pr("[",#x,"] = ["), pc(x);
+}
+
+using namespace output;
+
+#ifndef ATCODER_DSU_HPP
+#define ATCODER_DSU_HPP 1
+
+#include <algorithm>
+#include <cassert>
+#include <vector>
+
+namespace atcoder {
+
+// Implement (union by size) + (path compression)
+// Reference:
+// Zvi Galil and Giuseppe F. Italiano,
+// Data structures and algorithms for disjoint set union problems
+struct dsu {
+  public:
+    dsu() : _n(0) {}
+    explicit dsu(int n) : _n(n), parent_or_size(n, -1) {}
+
+    int merge(int a, int b) {
+        assert(0 <= a && a < _n);
+        assert(0 <= b && b < _n);
+        int x = leader(a), y = leader(b);
+        if (x == y) return x;
+        if (-parent_or_size[x] < -parent_or_size[y]) std::swap(x, y);
+        parent_or_size[x] += parent_or_size[y];
+        parent_or_size[y] = x;
+        return x;
+    }
+
+    bool same(int a, int b) {
+        assert(0 <= a && a < _n);
+        assert(0 <= b && b < _n);
+        return leader(a) == leader(b);
+    }
+
+    int leader(int a) {
+        assert(0 <= a && a < _n);
+        if (parent_or_size[a] < 0) return a;
+        return parent_or_size[a] = leader(parent_or_size[a]);
+    }
+
+    int size(int a) {
+        assert(0 <= a && a < _n);
+        return -parent_or_size[leader(a)];
+    }
+
+    std::vector<std::vector<int>> groups() {
+        std::vector<int> leader_buf(_n), group_size(_n);
+        for (int i = 0; i < _n; i++) {
+            leader_buf[i] = leader(i);
+            group_size[leader_buf[i]]++;
+        }
+        std::vector<std::vector<int>> result(_n);
+        for (int i = 0; i < _n; i++) {
+            result[i].reserve(group_size[i]);
+        }
+        for (int i = 0; i < _n; i++) {
+            result[leader_buf[i]].push_back(i);
+        }
+        result.erase(
+            std::remove_if(result.begin(), result.end(),
+                           [&](const std::vector<int>& v) { return v.empty(); }),
+            result.end());
+        return result;
+    }
+
+  private:
+    int _n;
+    // root node: -1 * component size
+    // otherwise: parent
+    std::vector<int> parent_or_size;
+};
+
+}  // namespace atcoder
+
+#endif  // ATCODER_DSU_HPP
+
+using namespace atcoder;
+
+void solve() {
+	int n, m;
+	cin >> n >> m;
+	vi h(n);
+	for (int i = 0;i < n;i++) cin >> h[i];
+	vector<pair<int, int>> edges;
+	for (int i = 0;i < m;i++) {
+		int x, y;
+		cin >> x >> y;
+		x--; y--;
+		if (h[x] < h[y]) swap(x, y);
+		edges.emplace_back(x, y);
+	}
+	sort(edges.begin(), edges.end(), [&](pii &a, pii &b) {
+		return h[a.first] < h[b.first];
+	});
+	int ep = 0;
+
+	int q;
+	cin >> q;
+	vvi qq;
+	for (int i = 0;i < q;i++) {
+		int a, b, e;
+		cin >> a >> b >> e;
+		a--; b--;
+		qq.push_back({a, b, e, i});
+	}
+	sort(qq.begin(), qq.end(), [&](vi &a, vi &b) {
+		return h[a[0]]+a[2] < h[b[0]]+b[2];
+	});
+
+	dsu cc(n);
+
+	vi ret(q);
+	for (int i = 0;i < q;i++) {
+		int a = qq[i][0];
+		int b = qq[i][1];
+		int e = qq[i][2];
+		int t = qq[i][3];
+		while (ep < edges.size() && h[edges[ep].first] <= h[a]+e) {
+			cc.merge(edges[ep].first, edges[ep].second);
+			ep++;
+		}
+		ret[t] = cc.same(a, b);
+	}
+
+	for (auto x: ret) {
+		cout << (x ? "YES" : "NO") << endl;
+	}
+	cout << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--) solve();
+    return 0;
+}
