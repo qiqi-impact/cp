2c89cede6b82d3be9652c5928489fe7867fbfd10
Qiqi Impact <qiqi.impact.official@gmail.com>
2023-09-04 16:52:26 -0700

    .

--- leetcode/996.py
+++ leetcode/996.py
@@ -9,8 +9,6 @@ class Solution:
         if n == 1:
             return 1
 
-        MOD = 10**9+7
-
         @cache
         def dfs(b, lst):
             if b == MX:
@@ -19,13 +17,11 @@ class Solution:
             for i in range(n):
                 if not (b & (1 << i)) and is_sq(nums[lst] + nums[i]):
                     ret += dfs(b ^ (1 << i), i)
-                    ret %= MOD
             return ret
 
         ct = Counter(nums)
 
         s = sum([dfs(1 << i, i) for i in range(n)])
         for v in ct.values():
-            s *= pow(factorial(v), -1, MOD)
-            s %= MOD
+            s //= factorial(v)
         return s
