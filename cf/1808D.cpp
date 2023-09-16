ef8734c05d4e0ad5b043b049fb63d79896a4e8f2
Qiqi Impact <qiqi.impact.official@gmail.com>
2023-09-16 11:13:50 -0700

    .

---
+++ cf/1808D.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using pii = pair<int, int>;
+
+namespace output {
+	void pr(int x) { cout << x; }
+	void pr(long x) { cout << x; }
+	void pr(ll x) { cout << x; }
+	void pr(unsigned x) { cout << x; }
+	void pr(unsigned long x) { cout << x; }
+	void pr(unsigned long long x) { cout << x; }
+	void pr(float x) { cout << x; }
+	void pr(double x) { cout << x; }
+	void pr(ld x) { cout << x; }
+	void pr(char x) { cout << x; }
+	void pr(const char* x) { cout << x; }
+	void pr(const string& x) { cout << x; }
+	void pr(bool x) { pr(x ? "true" : "false"); }
+	template<class T> void pr(const complex<T>& x) { cout << x; }
+	
+	template<class T1, class T2> void pr(const pair<T1,T2>& x);
+	template<class T> void pr(const T& x);
+	
+	template<class T, class... Ts> void pr(const T& t, const Ts&... ts) { 
+		pr(t); pr(ts...); 
+	}
+	template<class T1, class T2> void pr(const pair<T1,T2>& x) { 
+		pr("{",x.first,", ",x.second,"}"); 
+	}
+	template<class T> void pr(const T& x) { 
+		pr("{"); // const iterator needed for vector<bool>
+		bool fst = 1; for (const auto& a: x) pr(!fst?", ":"",a), fst = 0; 
+		pr("}");
+	}
+	
+	void ps() { pr("\n"); } // print w/ spaces
+	template<class T, class... Ts> void ps(const T& t, const Ts&... ts) { 
+		pr(t); if (sizeof...(ts)) pr(" "); ps(ts...); 
+	}
+	
+	void pc() { pr("]\n"); } // debug w/ commas
+	template<class T, class... Ts> void pc(const T& t, const Ts&... ts) { 
+		pr(t); if (sizeof...(ts)) pr(", "); pc(ts...); 
+	}
+	#define dbg(x...) pr("[",#x,"] = ["), pc(x);
+}
+
+using namespace output;
+
+void solve() {
+    int n, k;
+	cin >> n >> k;
+	unordered_map<int, vi> m[2];
+	for (int i = 0;i < n;i++) {
+		int x;
+		cin >> x;
+		m[i%2][x].push_back(i);
+	}
+	ll ret = (ll)k/2 * (n + 1 - k);
+	for (int i = 0;i < 2;i++) {
+		for (auto &[x, y] : m[i]) {
+			for (auto z : y) {
+				int l = lower_bound(y.begin(), y.end(), max(z+1, k-1-z)) - y.begin();
+				int r = lower_bound(y.begin(), y.end(), 1+min(min(n-1, z+k-1), 2 * (n-1) - z - (k-1))) - y.begin();
+				if (r > l) ret -= r - l;
+			}
+		}
+	}
+	cout << ret << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);solve();
+    return 0;
+}
