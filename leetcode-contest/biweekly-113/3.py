7137c1f4b3af6a159fbf243c1cb7281e231b78c9
Qiqi Impact <qiqi.impact.official@gmail.com>
2023-09-16 09:17:13 -0700

    .

--- leetcode-contest/biweekly-113/3.py
+++ leetcode-contest/biweekly-113/3.py
@@ -5,14 +5,6 @@ class Solution:
         for x, y in coordinates:
             for t in range(k+1):
                 u = k-t
-                if t^x not in ct:
-                    continue
-                if u^y not in ct[t^x]:
-                    continue
-                ret += ct[t^x][u^y]
-            if x not in ct:
-                ct[x] = {}
-            if y not in ct[x]:
-                ct[x][y] = 0
-            ct[x][y] += 1
+                ret += ct.get((t^x, u^y), 0)
+            ct[(x, y)] = ct.get((x, y), 0) + 1
         return ret
