e51d93d93a7ba2df9c4dc673144d65fad641fcf0
Qiqi Impact <qiqi.impact.official@gmail.com>
2023-10-24 23:05:45 -0700

    .

---
+++ leetcode/123.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        @cache
+        def dp(idx, state):
+            if state == 0 or idx == len(prices):
+                return 0
+            if state == 1 or state == 3:
+                return max(dp(idx+1, state), prices[idx] + dp(idx+1, state-1))
+            else:
+                return max(dp(idx+1, state), -prices[idx] + dp(idx+1, state-1))
+        return max(dp(0, 4), dp(0, 2))
