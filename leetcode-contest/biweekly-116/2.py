0cdc549e832a2d3d09e61da330d21b8cefe5e68a
Qiqi Impact <qiqi.impact.official@gmail.com>
2023-10-28 09:11:48 -0700

    .

--- leetcode-contest/biweekly-116/2.py
+++ leetcode-contest/biweekly-116/2.py
@@ -1,17 +1,8 @@
 class Solution:
     def minChanges(self, s: str) -> int:
-        l = [int(c) for c in s]
-        @cache
-        def dp(idx, c, ln):
-            if idx == len(s):
-                return 0
-            ret = inf
-            for x in [0, 1]:
-                df = int(x != l[idx])
-                if x == c:
-                    ret = min(ret, df + dp(idx+1, x, (ln+1)%2))
-                else:
-                    if not ln%2:
-                        ret = min(ret, df + dp(idx+1, x, 1))
-            return ret
-        return dp(0, None, 0)
+        changes = 0
+        for i in range(0, len(s) - 1, 2):
+            if s[i + 1] != s[i]:
+                changes += 1
+                
+        return changes
