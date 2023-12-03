3d7cbaa107f761f6c8a979ed2767edda86b14bd6
Qiqi Impact <qiqi.impact.official@gmail.com>
2023-12-02 20:16:23 -0800

    .

--- leetcode-contest/weekly-374/2.py
+++ leetcode-contest/weekly-374/2.py
@@ -5,11 +5,10 @@ class Solution:
         pt = 0
         ret = 0
         for i in range(1, target+1):
-            while pt < len(coins) and coins[pt] <= max(sf, i):
+            while pt < len(coins) and coins[pt] <= i:
                 sf += coins[pt]
                 pt += 1
             if i > sf:
                 ret += 1
                 sf += i
-                # print(i)
         return ret
