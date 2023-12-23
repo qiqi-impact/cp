e6c6a93323a583fd7ef2f6148a59c10e2e82e3db
Qiqi Impact <qiqi.impact.official@gmail.com>
2023-12-23 08:13:39 -0800

    .

--- leetcode-contest/biweekly-120/1.py
+++ leetcode-contest/biweekly-120/1.py
@@ -1,31 +1,16 @@
 class Solution:
     def incremovableSubarrayCount(self, nums: List[int]) -> int:
-        l = []
-        r = []
-        n = len(nums)
-        cur = -inf
-        for i in range(n):
-            if nums[i] > cur:
-                l.append(nums[i])
-            else:
-                break
-            cur = nums[i]
-            
-        cur = inf
-        for i in range(n-1, -1, -1):
-            if nums[i] < cur:
-                r.append(nums[i])
-            else:
-                break
-            cur = nums[i]
-        r = r[::-1]
-        
-        if len(l) == n:
-            return n * (n+1) // 2
-        
-        ret = len(r)+1
-        for x in l:
-            idx = bisect.bisect_left(r, x+1)
-            ret += len(r) - idx + 1
+        ret = 0
+        for i in range(len(nums)):
+            for j in range(i, len(nums)):
+                f = 0
+                cur = -inf
+                for k in range(len(nums)):
+                    if k < i or k > j:
+                        if nums[k] <= cur:
+                            f = 1
+                            break
+                        cur = nums[k]
+                if not f:
+                    ret += 1
         return ret
-            
