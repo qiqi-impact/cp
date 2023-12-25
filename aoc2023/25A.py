96dc7528311becf88401bd59aa4409ee31355458
Qiqi Impact <qiqi.impact.official@gmail.com>
2023-12-24 21:39:42 -0800

    .

--- aoc2023/25A.py
+++ aoc2023/25A.py
@@ -1,6 +1,10 @@
 from functools import cache
 from collections import defaultdict, deque
-import math
+import math, sys
+
+sys.setrecursionlimit(10000)
+
+import networkx
 
 def ints(s, split=' '):
     return [int(x) for x in s.split(split) if x]
@@ -16,17 +20,22 @@ digmap = {}
 for i, x in enumerate(digits):
     digmap[x] = i
 
-# dirmap = {
-#     'R': [0, 1],
-#     'L': [0, -1],
-#     'U': [-1, 0],
-#     'D': [1, 0],
-# }
-# L, R, U, D = [[0,-1], [0,1], [-1,0], [1,0]]
-# DIR = [R, D, L, U]
-
 lines = []
 with open("in") as f:
     for a in f.read().splitlines():
         lines.append(a)
-ln = len(lines)
+R = len(lines)
+
+g = networkx.Graph()
+comp = set()
+for x in lines:
+    a, b = x.split(': ')
+    comp.add(a)
+    for c in b.split(' '):
+        g.add_edge(a, c)
+        comp.add(c)
+
+for x, y in networkx.minimum_edge_cut(g):
+    g.remove_edge(x, y)
+
+print([len(c) for c in networkx.connected_components(g)])
