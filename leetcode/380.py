65e3da7a3d32e405b920c57d014978c37811efbe
Qiqi Impact <qiqi.impact.official@gmail.com>
2024-01-16 19:23:46 -0800

    .

--- leetcode/380.py
+++ leetcode/380.py
@@ -1,34 +1,31 @@
-import random
-
 class RandomizedSet:
 
     def __init__(self):
-        self.ct = 0
+        self.d = {}
         self.l = []
-        self.idx = {}
+        self.st = 0
 
     def insert(self, val: int) -> bool:
-        if val in self.idx:
+        if val in self.d:
             return False
         self.l.append(val)
-        self.idx[val] = len(self.l)-1
-        self.ct += 1
+        self.d[val] = len(self.l) - 1
         return True
 
     def remove(self, val: int) -> bool:
-        if val not in self.idx:
+        if val not in self.d:
             return False
-        i = len(self.l) - self.ct
-        j = self.idx[val]
-        v = self.l[i]
-        self.idx[v] = j
-        del self.idx[val]
-        self.l[j] = v
-        self.ct -= 1
+        idx = self.d[val]
+        if idx != self.st:
+            t = self.l[self.st]
+            self.l[idx], self.l[self.st] = self.l[self.st], self.l[idx]
+            self.d[t] = idx
+        self.st += 1
+        del self.d[val]
         return True
 
     def getRandom(self) -> int:
-        idx = random.randint(len(self.l) - self.ct, len(self.l)-1)
+        idx = random.randint(self.st, len(self.l) - 1)
         return self.l[idx]
 
 # Your RandomizedSet object will be instantiated and called as such:
