4da9ebaf35b934322cd6fce218def491504d846f
Qiqi Impact <qiqi.impact.official@gmail.com>
2024-01-20 20:11:13 -0800

    .

--- leetcode-contest/weekly-381/1.py
+++ leetcode-contest/weekly-381/1.py
@@ -1,11 +1,6 @@
 class Solution:
     def minimumPushes(self, word: str) -> int:
-        ct = Counter(word)
-        l = sorted([(-ct[x], x) for x in ct])
         ret = 0
-        ct = 0
-        for a, b in l:
-            a = -a
-            ret += (1+ct//8) * a
-            ct += 1
+        for i in range(len(word)):
+            ret += i//8 + 1
         return ret
