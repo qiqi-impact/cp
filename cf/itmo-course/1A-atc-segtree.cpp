c90ecc6dfb126b5fc20cda1c98f598da27fa5198
Qiqi Impact <qiqi.impact.official@gmail.com>
2024-04-16 14:08:01 -0700

    .

---
+++ cf/itmo-course/1A-atc-segtree.cpp
@@ -0,0 +1,216 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using pii = pair<int, int>;
+
+namespace output {
+	void pr(int x) { cout << x; }
+	void pr(long x) { cout << x; }
+	void pr(ll x) { cout << x; }
+	void pr(unsigned x) { cout << x; }
+	void pr(unsigned long x) { cout << x; }
+	void pr(unsigned long long x) { cout << x; }
+	void pr(float x) { cout << x; }
+	void pr(double x) { cout << x; }
+	void pr(ld x) { cout << x; }
+	void pr(char x) { cout << x; }
+	void pr(const char* x) { cout << x; }
+	void pr(const string& x) { cout << x; }
+	void pr(bool x) { pr(x ? "true" : "false"); }
+	template<class T> void pr(const complex<T>& x) { cout << x; }
+	
+	template<class T1, class T2> void pr(const pair<T1,T2>& x);
+	template<class T> void pr(const T& x);
+	
+	template<class T, class... Ts> void pr(const T& t, const Ts&... ts) { 
+		pr(t); pr(ts...); 
+	}
+	template<class T1, class T2> void pr(const pair<T1,T2>& x) { 
+		pr("{",x.first,", ",x.second,"}"); 
+	}
+	template<class T> void pr(const T& x) { 
+		pr("{"); // const iterator needed for vector<bool>
+		bool fst = 1; for (const auto& a: x) pr(!fst?", ":"",a), fst = 0; 
+		pr("}");
+	}
+	
+	void ps() { pr("\n"); } // print w/ spaces
+	template<class T, class... Ts> void ps(const T& t, const Ts&... ts) { 
+		pr(t); if (sizeof...(ts)) pr(" "); ps(ts...); 
+	}
+	
+	void pc() { pr("]\n"); } // debug w/ commas
+	template<class T, class... Ts> void pc(const T& t, const Ts&... ts) { 
+		pr(t); if (sizeof...(ts)) pr(", "); pc(ts...); 
+	}
+	#define dbg(x...) pr("[",#x,"] = ["), pc(x);
+}
+
+using namespace output;
+
+#if __cplusplus >= 201703L
+template <class S, auto op, auto e> struct segtree {
+    static_assert(std::is_convertible_v<decltype(op), std::function<S(S, S)>>,
+                  "op must work as S(S, S)");
+    static_assert(std::is_convertible_v<decltype(e), std::function<S()>>,
+                  "e must work as S()");
+
+#else
+template <class S, S (*op)(S, S), S (*e)()> struct segtree {
+#endif
+
+    // @return same with std::bit::bit_ceil
+    unsigned int bit_ceil(unsigned int n) {
+        unsigned int x = 1;
+        while (x < (unsigned int)(n)) x *= 2;
+        return x;
+    }
+
+    // @param n `1 <= n`
+    // @return same with std::bit::countr_zero
+    int countr_zero(unsigned int n) {
+#ifdef _MSC_VER
+        unsigned long index;
+        _BitScanForward(&index, n);
+        return index;
+#else
+        return __builtin_ctz(n);
+#endif
+    }
+
+  public:
+    segtree() : segtree(0) {}
+    explicit segtree(int n) : segtree(std::vector<S>(n, e())) {}
+    explicit segtree(const std::vector<S>& v) : _n(int(v.size())) {
+        size = (int)bit_ceil((unsigned int)(_n));
+        log = countr_zero((unsigned int)size);
+        d = std::vector<S>(2 * size, e());
+        for (int i = 0; i < _n; i++) d[size + i] = v[i];
+        for (int i = size - 1; i >= 1; i--) {
+            update(i);
+        }
+    }
+
+    void set(int p, S x) {
+        assert(0 <= p && p < _n);
+        p += size;
+        d[p] = x;
+        for (int i = 1; i <= log; i++) update(p >> i);
+    }
+
+    S get(int p) const {
+        assert(0 <= p && p < _n);
+        return d[p + size];
+    }
+
+    S prod(int l, int r) const {
+        assert(0 <= l && l <= r && r <= _n);
+        S sml = e(), smr = e();
+        l += size;
+        r += size;
+
+        while (l < r) {
+            if (l & 1) sml = op(sml, d[l++]);
+            if (r & 1) smr = op(d[--r], smr);
+            l >>= 1;
+            r >>= 1;
+        }
+        return op(sml, smr);
+    }
+
+    S all_prod() const { return d[1]; }
+
+    template <bool (*f)(S)> int max_right(int l) const {
+        return max_right(l, [](S x) { return f(x); });
+    }
+    template <class F> int max_right(int l, F f) const {
+        assert(0 <= l && l <= _n);
+        assert(f(e()));
+        if (l == _n) return _n;
+        l += size;
+        S sm = e();
+        do {
+            while (l % 2 == 0) l >>= 1;
+            if (!f(op(sm, d[l]))) {
+                while (l < size) {
+                    l = (2 * l);
+                    if (f(op(sm, d[l]))) {
+                        sm = op(sm, d[l]);
+                        l++;
+                    }
+                }
+                return l - size;
+            }
+            sm = op(sm, d[l]);
+            l++;
+        } while ((l & -l) != l);
+        return _n;
+    }
+
+    template <bool (*f)(S)> int min_left(int r) const {
+        return min_left(r, [](S x) { return f(x); });
+    }
+    template <class F> int min_left(int r, F f) const {
+        assert(0 <= r && r <= _n);
+        assert(f(e()));
+        if (r == 0) return 0;
+        r += size;
+        S sm = e();
+        do {
+            r--;
+            while (r > 1 && (r % 2)) r >>= 1;
+            if (!f(op(d[r], sm))) {
+                while (r < size) {
+                    r = (2 * r + 1);
+                    if (f(op(d[r], sm))) {
+                        sm = op(d[r], sm);
+                        r--;
+                    }
+                }
+                return r + 1 - size;
+            }
+            sm = op(d[r], sm);
+        } while ((r & -r) != r);
+        return 0;
+    }
+
+  private:
+    int _n, size, log;
+    std::vector<S> d;
+
+    void update(int k) { d[k] = op(d[2 * k], d[2 * k + 1]); }
+};
+
+ll op(ll a, ll b) {
+    return a + b;
+}
+
+ll ex() { return 0; }
+
+void solve() {
+    int n, q;
+	cin >> n >> q;
+    vll a(n);
+    for (int i = 0;i < n;i++) cin >> a[i];
+    segtree<ll, op, ex> st(a);
+	for (int i = 0;i < q;i++) {
+		ll t, l, r;
+		cin >> t >> l >> r;
+		if (t == 1) {
+			st.set(l, r);
+		} else {
+			cout << st.prod(l, r) << '\n';
+		}
+	}
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(false);
+    solve();
+    return 0;
+}
