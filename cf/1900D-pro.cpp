7b0291f0578394168e71776b8eba3142a3e0948b
Qiqi Impact <qiqi.impact.official@gmail.com>
2024-04-17 22:18:55 -0700

    .

---
+++ cf/1900D-pro.cpp
@@ -0,0 +1,129 @@
+#include "bits/stdc++.h"
+#pragma GCC optimize ("O3")
+#pragma GCC target ("sse4")
+
+#define DEBUG 1
+
+using namespace std;
+ 
+typedef long long ll;
+typedef long double ld;
+typedef complex<ld> cd;
+ 
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pl;
+typedef pair<ld,ld> pd;
+ 
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<cd> vcd;
+ 
+template<class T> using pq = priority_queue<T>;
+template<class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+ 
+#define FOR(i, a, b) for (int i=a; i<(b); i++)
+#define F0R(i, a) for (int i=0; i<(a); i++)
+#define FORd(i,a,b) for (int i = (b)-1; i >= a; i--)
+#define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
+#define trav(a,x) for (auto& a : x)
+#define uid(a, b) uniform_int_distribution<int>(a, b)(rng)
+ 
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+#define ins insert
+ 
+template<class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+ 
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+ 
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+ 
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ", "; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? ", " : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifdef DEBUG
+#define dbg(x...) cerr << "\e[91m"<<__func__<<":"<<__LINE__<<" [" << #x << "] = ["; _print(x); cerr << "\e[39m" << endl;
+#else
+#define dbg(x...)
+#endif
+ 
+ 
+const int MOD = 1000000007;
+const char nl = '\n';
+const int MX = 100001; 
+ 
+ll val[MX];
+ 
+ll cnt[MX];
+void solve() {
+    int N; cin >> N;
+    vi A(N); F0R(i, N) cin >> A[i];
+    F0R(i, MX) cnt[i] = 0;
+    sort(all(A));
+    ll ans = 0;
+    F0R(i, N) {
+        FOR(j, 1, MX) {
+            if (j*j > A[i]) break;
+            if (A[i]%j) continue;
+            ans += cnt[j] * val[j] * (N - i - 1);
+            dbg(A[i], j, cnt[j], val[j]);
+            cnt[j]++;
+            if (j*j != A[i]) {
+                ans += cnt[A[i]/j] * val[A[i]/j] * (N-i-1);
+                dbg(A[i], A[i]/j, cnt[A[i]/j], val[A[i]/j]);
+ 
+                cnt[A[i]/j]++;
+            }
+        }
+    }
+    cout << ans << nl;
+ 
+}
+ 
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    val[1] = 1;
+    FOR(i, 2, MX) {
+        val[i] = i-1;
+        FOR(j, 2, MX) {
+            if (j*j > i) break;
+            if (i%j) continue;
+            val[i] -= val[j];
+            if (j*j != i) val[i] -= val[i/j];
+        }
+    }
+ 
+    int T = 1;
+    cin >> T;
+    while(T--) {
+        solve();
+    }
+ 
+	return 0;
+}
---
+++ cf/1900D.cpp
@@ -0,0 +1,145 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using pii = pair<int, int>;
+
+namespace output {
+	void pr(int x) { cout << x; }
+	void pr(long x) { cout << x; }
+	void pr(ll x) { cout << x; }
+	void pr(unsigned x) { cout << x; }
+	void pr(unsigned long x) { cout << x; }
+	void pr(unsigned long long x) { cout << x; }
+	void pr(float x) { cout << x; }
+	void pr(double x) { cout << x; }
+	void pr(ld x) { cout << x; }
+	void pr(char x) { cout << x; }
+	void pr(const char* x) { cout << x; }
+	void pr(const string& x) { cout << x; }
+	void pr(bool x) { pr(x ? "true" : "false"); }
+	template<class T> void pr(const complex<T>& x) { cout << x; }
+	
+	template<class T1, class T2> void pr(const pair<T1,T2>& x);
+	template<class T> void pr(const T& x);
+	
+	template<class T, class... Ts> void pr(const T& t, const Ts&... ts) { 
+		pr(t); pr(ts...); 
+	}
+	template<class T1, class T2> void pr(const pair<T1,T2>& x) { 
+		pr("{",x.first,", ",x.second,"}"); 
+	}
+	template<class T> void pr(const T& x) { 
+		pr("{"); // const iterator needed for vector<bool>
+		bool fst = 1; for (const auto& a: x) pr(!fst?", ":"",a), fst = 0; 
+		pr("}");
+	}
+	
+	void ps() { pr("\n"); } // print w/ spaces
+	template<class T, class... Ts> void ps(const T& t, const Ts&... ts) { 
+		pr(t); if (sizeof...(ts)) pr(" "); ps(ts...); 
+	}
+	
+	void pc() { pr("]\n"); } // debug w/ commas
+	template<class T, class... Ts> void pc(const T& t, const Ts&... ts) { 
+		pr(t); if (sizeof...(ts)) pr(", "); pc(ts...); 
+	}
+	#define dbg(x...) pr("[",#x,"] = ["), pc(x);
+}
+
+using namespace output;
+
+int fp[Nmax]; //fp = first prime, i == fp[i] means prime
+int np[Nmax]; //np = num primes
+vi makeSieve(int lim) {
+  vi ret;
+  for(int i=2;i<=lim;++i) {
+    if(!fp[i]) {
+      ret.push_back(i);
+      for(int j=i;i*j<=lim;++j) {
+        if(!fp[1LL*i*j]) fp[1LL*i*j] = i;
+        ++np[j];
+      }
+      fp[i] = i;
+    }
+  }
+  return ret;
+}
+vi getPrimes(int x) {
+  vi ret;
+  while(x > 1) {
+    int p = fp[x];
+    ret.push_back(p);
+    while(x % p == 0) x /= p;
+  }
+  return ret;
+}
+
+void solve() {
+    int n;
+	cin >> n;
+	vi a(n);
+	int mx = 0;
+	for (int i = 0;i < n;i++) {
+		cin >> a[i];
+		mx = max(mx, a[i]);
+	}
+	sort(a.begin(), a.end());
+	map<int, int> above, freq;
+	set<int> factors;
+	for (int i = n - 1;i >= 0;i--) {
+		freq[a[i]]++;
+
+		for (int j = 1;j * j <= a[i];j++) {
+			factors.insert(j);
+			factors.insert(a[i] / j);
+		}
+
+		if (i == n - 1 || a[i] < a[i+1]) {
+			above[a[i]] = n - i - 1;
+		}
+	}
+	map<int, map<int, ll>> hits;
+	for (auto i : factors) {
+		int below = 0;
+		for (int j = i;j <= mx;j += i) {
+			ll ct = 0;
+			for (int k = 0;k < freq[j];k++) {
+				ct += (ll)(below + k) * (above[j] + freq[j] - k - 1);
+			}
+			if (freq.count(j)) below += freq[j];
+			if (ct) hits[j][i] = ct;
+		}
+	}
+	// dbg(hits);
+	ll ret = 0;
+	for (auto &[i, hi] : hits) {
+		vi ks;
+		for (auto &[x, y] : hi) {
+			ks.push_back(x);
+		}
+		sort(ks.rbegin(), ks.rend());
+		for (auto x : ks) {
+			for (int j = 2*x;j <= mx;j += x) {
+				if (hits[i].count(j)) {
+					hits[i][x] -= hits[i][j];
+				}
+			}
+			// dbg(i, x, (ll)x * hits[i][x]);
+			ret += (ll)x * hits[i][x];
+		}
+	}
+	cout << ret << endl;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(false);
+    int t;
+    cin >> t;
+    while (t--) solve();
+    return 0;
+}
