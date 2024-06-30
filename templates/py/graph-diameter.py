db12ec304d496bbd020e84ae879f2e5654799f69
Qiqi Impact <qiqi.impact.official@gmail.com>
2024-06-30 13:11:46 -0700

    .

---
+++ templates/py/graph-diameter.py
@@ -0,0 +1,32 @@
+def diam(n, edges):
+    adj, visited = [[] for i in range(n + 1)], [0 for i in range(n + 1)]
+    for x, y in edges:
+        adj[x].append(y)
+        adj[y].append(x)
+    maxCount = 0
+    far = 0
+    
+    def dfsUtil(node, count):
+        nonlocal visited, far, maxCount, adj
+        visited[node] = 1
+        count += 1
+        for i in adj[node]:
+            if (visited[i] == 0):
+                if (count >= maxCount):
+                    maxCount = count
+                    far = i
+                dfsUtil(i, count)
+
+    def dfs(node, n):
+        count = 0
+        for i in range(n + 1):
+            visited[i] = 0
+        dfsUtil(node, count + 1)
+
+    def diameter(n):
+        nonlocal adj, maxCount
+        dfs(1, n)
+        dfs(far, n)
+        return maxCount
+    
+    return diameter(n) - 1
