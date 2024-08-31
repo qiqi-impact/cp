40718d4e51c2d460b442cdf0be397f1adf693e44
Qiqi Impact <qiqi.impact.official@gmail.com>
2024-08-31 10:03:05 -0700

    .

--- leetcode-contest/biweekly-138/4.py
+++ leetcode-contest/biweekly-138/4.py
@@ -1,9 +1,12 @@
 class Solution:
     def minDamage(self, p: int, d: List[int], h: List[int]) -> int:
+        class cmp(int):
+            def __lt__(self, other):
+                return h[self] * d[other] < h[other] * d[self]
         n = len(d)
         h = [(x + p - 1) // p for x in h]
         l = list(range(n))
-        l.sort(key=lambda x:-d[x]/h[x])
+        l.sort(key=cmp)
         cur = sum(d)
         ret = 0
         for i in l:
