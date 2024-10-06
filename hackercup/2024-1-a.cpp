f8d0146ad341486351666a66257b1d96fc57110e
Qiqi Impact <qiqi.impact.official@gmail.com>
2024-10-05 17:39:00 -0700

    .

---
+++ hackercup/2024-1-a.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using pii = pair<int, int>;
+
+namespace output {
+	void pr(int x) { cout << x; }
+	void pr(long x) { cout << x; }
+	void pr(ll x) { cout << x; }
+	void pr(unsigned x) { cout << x; }
+	void pr(unsigned long x) { cout << x; }
+	void pr(unsigned long long x) { cout << x; }
+	void pr(float x) { cout << x; }
+	void pr(double x) { cout << x; }
+	void pr(ld x) { cout << x; }
+	void pr(char x) { cout << x; }
+	void pr(const char* x) { cout << x; }
+	void pr(const string& x) { cout << x; }
+	void pr(bool x) { pr(x ? "true" : "false"); }
+	template<class T> void pr(const complex<T>& x) { cout << x; }
+	
+	template<class T1, class T2> void pr(const pair<T1,T2>& x);
+	template<class T> void pr(const T& x);
+	
+	template<class T, class... Ts> void pr(const T& t, const Ts&... ts) { 
+		pr(t); pr(ts...); 
+	}
+	template<class T1, class T2> void pr(const pair<T1,T2>& x) { 
+		pr("{",x.first,", ",x.second,"}"); 
+	}
+	template<class T> void pr(const T& x) { 
+		pr("{"); // const iterator needed for vector<bool>
+		bool fst = 1; for (const auto& a: x) pr(!fst?", ":"",a), fst = 0; 
+		pr("}");
+	}
+	
+	void ps() { pr("\n"); } // print w/ spaces
+	template<class T, class... Ts> void ps(const T& t, const Ts&... ts) { 
+		pr(t); if (sizeof...(ts)) pr(" "); ps(ts...); 
+	}
+	
+	void pc() { pr("]\n"); } // debug w/ commas
+	template<class T, class... Ts> void pc(const T& t, const Ts&... ts) { 
+		pr(t); if (sizeof...(ts)) pr(", "); pc(ts...); 
+	}
+	#define dbg(x...) pr("[",#x,"] = ["), pc(x);
+}
+
+using namespace output;
+
+ld solve() {
+    ld left = 0, right = (ld)1e18;
+    int n;
+    cin >> n;
+    bool fail = false;
+    for (int i = 0;i < n;i++) {
+        ld a, b;
+        cin >> a >> b;
+        left = max(left, (ld)(i + 1) / b);
+        if (a) right = min(right, (ld)(i + 1) / a);
+        if (left > right) {
+            fail = true;
+        }
+    }
+    if (fail) return -1;
+    return left;
+}
+
+int main() {
+    freopen("subsonic_subway_input.txt", "r", stdin);
+    freopen("1a.txt", "w", stdout);
+    cout << fixed << setprecision(20);
+    int t;
+    cin >> t;
+    for (int i = 1;i <= t;i++) {
+        cout << "Case #" << i << ": " << solve() << endl;
+    }
+    return 0;
+}
