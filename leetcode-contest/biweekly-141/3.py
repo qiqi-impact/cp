95e06ca0a8fb708be9844ca41f05251a1a1fccf9
Qiqi Impact <qiqi.impact.official@gmail.com>
2024-10-12 09:20:23 -0700

    .

--- leetcode-contest/biweekly-141/3.py
+++ leetcode-contest/biweekly-141/3.py
@@ -1,14 +1,16 @@
 class Solution:
     def maxRemovals(self, s: str, p: str, t: List[int]) -> int:
         t = set(t)
-        dp = [[-inf for _ in range(len(p)+1)] for _ in range(len(s)+1)]
-        for j in range(len(p)+1):
-            dp[len(s)][j] = -inf if j != len(p) else 0
-        for i in range(len(s)-1, -1, -1):
-            dp[i][len(p)] = int(i in t) + dp[i+1][len(p)]
-        for i in range(len(s)-1, -1, -1):
-            for j in range(len(p)-1, -1, -1):
-                dp[i][j] = int(i in t) + dp[i+1][j]
-                if s[i] == p[j]:
-                    dp[i][j] = max(dp[i][j], dp[i+1][j+1])
-        return dp[0][0]
+        @cache
+        def dp(si, pi):
+            if si == len(s):
+                return -inf if pi != len(p) else 0
+            if pi == len(p):
+                return int(si in t) + dp(si+1, pi)
+            ret = int(si in t) + dp(si+1, pi)
+            if s[si] == p[pi]:
+                ret = max(ret, dp(si+1, pi+1))
+            return ret
+        r = dp(0, 0)
+        dp.cache_clear()
+        return r
