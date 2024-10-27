1c2aed6edac41b09f24d47d6645168ec52212b99
Qiqi Impact <qiqi.impact.official@gmail.com>
2024-10-26 21:48:55 -0700

    .

---
+++ templates/py/matrix-fast-mult.py
@@ -0,0 +1,26 @@
+class MatrixFastPower:
+    def __init__(self):
+        return
+
+    @staticmethod
+    def _matrix_mul(a, b, mod=10 ** 9 + 7):
+        n = len(a)
+        res = [[0] * n for _ in range(n)]
+        for i in range(n):
+            for k in range(n):
+                if a[i][k]:
+                    for j in range(n):
+                        res[i][j] = (res[i][j] + a[i][k] * b[k][j]) % mod
+        return res
+
+    def matrix_pow(self, base, p, mod=10 ** 9 + 7):
+        n = len(base)
+        ans = [[0] * n for _ in range(n)]
+        for i in range(n):
+            ans[i][i] = 1
+        while p:
+            if p & 1:
+                ans = self._matrix_mul(ans, base, mod)
+            base = self._matrix_mul(base, base, mod)
+            p >>= 1
+        return ans
--- templates/py/matrix_mult.py
+++ templates/py/matrix_mult.py
@@ -5,28 +5,3 @@ def mat_mul(X, Y):
             for k in range(len(Y)):
                 result[i][j] += X[i][k] * Y[k][j]
     return result
-
-class MatrixFastPower:
-    def __init__(self):
-        return
-
-    @staticmethod
-    def _matrix_mul(a, b, mod=10 ** 9 + 7):
-        n = len(a)
-        res = [[0] * n for _ in range(n)]
-        for i in range(n):
-            for j in range(n):
-                res[i][j] = sum(a[i][k] * b[k][j] for k in range(n)) % mod
-        return res
-
-    def matrix_pow(self, base, p, mod=10 ** 9 + 7):
-        n = len(base)
-        ans = [[0] * n for _ in range(n)]
-        for i in range(n):
-            ans[i][i] = 1
-        while p:
-            if p & 1:
-                ans = self._matrix_mul(ans, base, mod)
-            base = self._matrix_mul(base, base, mod)
-            p >>= 1
-        return ans
