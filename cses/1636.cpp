3aadcacdb24a7d4d8e31857831ea0ef4ce47ccce
Qiqi Impact <qiqi.impact.official@gmail.com>
2024-11-13 22:59:03 -0800

    .

--- cses/1636.cpp
+++ cses/1636.cpp
@@ -69,12 +69,11 @@ void solve() {
 	for (int i = 0;i < n;i++) cin >> a[i];
 	for (int i = 1;i <= n;i++) {
 		for (int j = 0;j <= x;j++) dp[i%2][j] = 0;
+		vll acc(a[i-1], 0);
 		for (int j = 0;j <= x;j++) {
-			if (!dp[(i-1)%2][j]) continue;
-			for (int k = j;k <= x;k += a[i-1]) {
-				dp[i%2][k] += dp[(i-1)%2][j];
-				dp[i%2][k] %= MOD;
-			}
+			acc[j % a[i-1]] += dp[(i-1)%2][j];
+			acc[j % a[i-1]] %= MOD;
+			dp[i%2][j] = acc[j % a[i-1]];
 		}
 	}
 	cout << dp[n%2][x] << endl;
