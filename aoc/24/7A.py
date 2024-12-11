f20c9e458d8191a7ca58cb0e19e8dc51b147ec7c
Qiqi Impact <qiqi.impact.official@gmail.com>
2024-12-11 00:13:49 -0800

    .

--- aoc/24/7A.py
+++ aoc/24/7A.py
@@ -21,9 +21,32 @@ def colon(s):
 def merge(l):
     return ''.join([str(x) for x in l])
 
+
+def dfs(sm, cur, idx, ll):
+    if idx == len(ll):
+        return sm == cur
+    if cur + ll[idx] <= sm:
+        if dfs(sm, cur + ll[idx], idx + 1, ll):
+            return True
+    if cur * ll[idx] <= sm:
+        if dfs(sm, cur * ll[idx], idx + 1, ll):
+            return True
+    if int(str(cur)+str(ll[idx])) <= sm:
+        if dfs(sm, int(str(cur)+str(ll[idx])), idx + 1, ll):
+            return True
+    return False
+
+
 lines = []
+ret = 0
 with open("in") as f:
     for a in f.read().splitlines():
-        lines.append(a)
+        # lines.append(a)
+        s, l = a.split(': ')
+        s = int(s)
+        l = ints(l)
+        if dfs(s, l[0], 1, l):
+            ret += s
+print(ret)
 
         
