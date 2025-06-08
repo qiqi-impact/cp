9dfd6ec79b5b03cbe85bdae2bf0010ad4fe920f7
Qiqi Impact <qiqi.impact.official@gmail.com>
2025-06-07 21:55:47 -0700

    .

--- leetcode-contest/weekly-453/3.py
+++ leetcode-contest/weekly-453/3.py
@@ -4,23 +4,18 @@ class Solution:
     def countPartitions(self, nums: List[int], k: int) -> int:
         MOD = 10**9+7
         n = len(nums)
-
         sl = SortedList()
-        q = deque()
-
+        j = -1
         dp = [1]
-        sdp = [0, 1]
+        sdp = 1
         for i, x in enumerate(nums):
-            q.append((x, i))
-            sl.add((x, i))
-            while sl and sl[-1][0] - sl[0][0] > k:
-                a, b = q.popleft()
-                sl.discard((a, b))
-            
-            first = q[0][1]
-            v = (sdp[i + 1] - sdp[first]) % MOD
-            dp.append(v)
-            sdp.append((sdp[-1] + dp[-1]) % MOD)
-            # print(i, q, dp, sdp)
+            sl.add(x)
+            while sl and sl[-1] - sl[0] > k:
+                j += 1
+                sl.discard(nums[j])
+                sdp -= dp[j]
+                sdp %= MOD
+            dp.append(sdp)
+            sdp += dp[-1]
+            sdp %= MOD
         return dp[-1]
-            
